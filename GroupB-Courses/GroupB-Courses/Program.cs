2b5cf66befe7849ef1043db26029deddee6207c2
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-24 15:46:53 +0500

    Started Entity Framework

--- GroupB-Courses/GroupB-Courses/Program.cs
+++ GroupB-Courses/GroupB-Courses/Program.cs
@@ -6,72 +6,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GroupB_Courses
-{
-    public class Params : IEnumerable<int>
-    {
-        //link pad
-
-
-        private int a, b, c;
-
-        public Params(int a, int b, int c)
-        {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-        }
-
-        public IEnumerator<int> GetEnumerator()
-        {
-            yield return a;
-            yield return b;
-            yield return c;
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-    }
-
-    public class Person
-    {
-        private string firstName, middleName, lastName;
-
-        public Person(string firstName, string middleName, string lastName)
-        {
-            this.firstName = firstName;
-            this.middleName = middleName;
-            this.lastName = lastName;
-        }
-
-        public IEnumerable<string> Names
-        {
-            get
-            {
-                yield return firstName;
-                yield return middleName;
-                yield return lastName;
-            }
-        }
-        class Program
+{       class Program
         {
             static void Main(string[] args)
             {
-                var p = new Params(1, 2, 3);
-
-                foreach (var x in p)
-                {
-                    Console.WriteLine(x);
-                }
-
-                var person = new Person("Vladimir", "Ilyitch", "Lenin");
-
-                foreach (var name in person.Names)
-                {
-                    Console.WriteLine(name);
-                }
+                
             }
         }
-    }
+    
 }
