d14be22834abd5b90d8348a73bd061ed67483e58
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-24 09:56:30 +0500

    Completed Lesson 3 How linq works

--- GroupB-Courses/GroupB-Courses/Program.cs
+++ GroupB-Courses/GroupB-Courses/Program.cs
@@ -10,6 +10,17 @@ namespace GroupB_Courses
     {
         static void Main(string[] args)
         {
+            //Linq
+            //language integrated query
+            //a trchnology for searching and filyering datasets
+            //it is anythig taht can be enumerate
+            //extension method 
+            //query expression syntax
+            //linq operators are called higher order function
+            //function whic are taking other functions
+            //prediacte is a function which take type T and return boolean
+            //eg where
+            //
         }
     }
 }
