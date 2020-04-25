25ae5f88be89ea7cdd937423bfb255fe00273257
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-25 18:55:58 +0500

    Completed Lesson 17. Importing Stored Procedures

--- GroupB-Courses/DbFirst/Program.cs
+++ GroupB-Courses/DbFirst/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,12 @@ namespace DbFirst
         static void Main(string[] args)
         {
             //meta deta :refer to assemblies  
+            var dbContext = new PlutoDbContext();
+            var courses=dbContext.GetCourses();
+            foreach (var cou in courses)
+            {
+                Console.WriteLine(cou.Title);
+            }
         }
     }
 }
