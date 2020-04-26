426e265dc70cf5e251848c497d49bd8d85bb450f
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-26 22:14:20 +0500

    Completed Lesson 31. Seeding Database

--- GroupB-Courses/CodeFirstExistingDatabase/Migrations/Configuration.cs
+++ GroupB-Courses/CodeFirstExistingDatabase/Migrations/Configuration.cs
@@ -1,7 +1,9 @@
 namespace CodeFirstExistingDatabase.Migrations
 {
     using System;
+    using System.Collections.ObjectModel;
     using System.Data.Entity;
+    using System.Data.Entity.Core.Metadata.Edm;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
@@ -10,10 +12,20 @@ namespace CodeFirstExistingDatabase.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+           
         }
 
         protected override void Seed(CodeFirstExistingDatabase.PlutoContext context)
         {
+            context.Authors.AddOrUpdate(a => a.Name,
+                new Author
+                {
+                    Name = "Author 1",
+                    Cours = new Collection<Cour>() { new Cour() { Name = "Course for Author 1", Description = "Description" } }
+
+
+                });
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
