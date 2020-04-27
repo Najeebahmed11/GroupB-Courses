88472b342b5ea21f4963e0f2da346ff6b5e58b88
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-27 17:22:53 +0500

    Completed Lesson 42. Organizing Fluent API Configurations

--- GroupB-Courses/FluentAPI/FluentAPI/PlutoContext.cs
+++ GroupB-Courses/FluentAPI/FluentAPI/PlutoContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using FluentAPI.EntityConfiguration;
+using System.Data.Entity;
 
 namespace DataAnnotations
 {
@@ -14,32 +15,7 @@ namespace DataAnnotations
         public virtual DbSet<Tag> Tags { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Course>()
-                .Property(c => c.Name)
-                .IsRequired()
-                .HasMaxLength(255);
-            modelBuilder.Entity<Course>()
-                .Property(c => c.Description)
-                .IsRequired()
-                .HasMaxLength(2000);
-            modelBuilder.Entity<Course>()
-                .HasRequired(c => c.Author)
-                .WithMany(a => a.Courses)
-                .HasForeignKey(c => c.AuthorId)
-                .WillCascadeOnDelete(false);
-            modelBuilder.Entity<Course>()
-                .HasMany(c => c.Tags)
-                .WithMany(t => t.Courses)
-                .Map(m => 
-                    {
-                        m.ToTable("CourseTags");
-                        m.MapLeftKey("CourseId");
-                        m.MapRightKey("TagId");
-                    });
-            modelBuilder.Entity<Course>()
-                .HasRequired(c => c.Cover)
-                .WithRequiredPrincipal(c => c.Course);
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add(new CourseConfiguration());
         }
     }
 }
