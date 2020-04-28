033a98ccb10a139845816ed933a307b907af23ad
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-28 16:44:27 +0500

    Completed Lesson 59. Explicit Loading

--- GroupB-Courses/Queries/Queries/Program.cs
+++ GroupB-Courses/Queries/Queries/Program.cs
@@ -9,8 +9,10 @@ namespace Queries
         {
 
             var Context = new PlutoContext();
-            var courses = Context.Courses.Include(c=>c.Author).ToList();
-            foreach(var course in courses)
+            var authors = Context.Authors.Single(a=>a.Id==1);
+            Context.Entry(authors).Collection(a => a.Courses).Query().Where(c=>c.FullPrice==0).Load();
+            Context.Courses.Where(c => c.AuthorId == authors.Id && c.FullPrice==0).Load();
+            foreach(var course in authors.Courses)
             {
                 Console.WriteLine("{0} by {1}", course.Name, course.Author.Name);
             }
