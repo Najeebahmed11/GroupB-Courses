6aff4288872727bdcd77082a08c663063ba82d76
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-28 17:01:19 +0500

    Completed Lesson 60. Exercises

--- GroupB-Courses/Vidzy/Vidzy/Program.cs
+++ GroupB-Courses/Vidzy/Vidzy/Program.cs
@@ -1,5 +1,5 @@
-﻿
-using System;
+﻿using System;
+using System.Data.Entity;
 using System.Linq;
 
 namespace Vidzy
@@ -8,85 +8,38 @@ namespace Vidzy
     {
         static void Main(string[] args)
         {
+            // To enable lazy loading, you need to declare navigation properties
+            // as virtual. Look at the Video class.
+
             var context = new VidzyContext();
-
-            // Action movies sorted by name
-            Console.WriteLine();
-            Console.WriteLine("ACTION MOVIES SORTED BY NAME");
-            var actionMovies = context.Videos
-                .Where(v => v.Genre.Name == "Action")
-                .OrderBy(v => v.Name);
-
-            foreach (var v in actionMovies)
-                Console.WriteLine(v.Name);
-
-            // Gold drama movies sorted by release date (newest first)
-            var dramaMovies = context.Videos
-                .Where(v => v.Genre.Name == "Drama" && v.Classification == Classification.Gold)
-                .OrderByDescending(v => v.ReleaseDate);
+            var videos = context.Videos.ToList();
 
             Console.WriteLine();
-            Console.WriteLine("GOLD DRAMA MOVIES SORTED BY RELEASE DATE (NEWEST FIRST)");
-            foreach (var v in dramaMovies)
-                Console.WriteLine(v.Name);
+            Console.WriteLine("LAZY LOADING");
+            foreach (var v in videos)
+                Console.WriteLine("{0} ({1})", v.Name, v.Genre.Name);
 
-            // All movies projected into an anonymous type
-            var projected = context.Videos
-                .Select(v => new {MovieName = v.Name, Genre = v.Genre.Name});
-
-            Console.WriteLine();
-            Console.WriteLine("ALL MOVIES PROJECTED INTO AN ANONYMOUS TYPE");
-            foreach (var v in projected)
-                Console.WriteLine(v.MovieName);
-            //loading objects
-            // All movies grouped by classification
-            var groups = context.Videos
-                .GroupBy(v => v.Classification)
-                .Select(g => new
-                {
-                    Classification = g.Key.ToString(), 
-                    Videos = g.OrderBy(v => v.Name)
-                });
+            // Eager loading
+            var videosWithGenres = context.Videos.Include(v => v.Genre).ToList();
 
             Console.WriteLine();
-            Console.WriteLine("ALL MOVIES GROUPED BY CLASSIFICATION");
-            foreach (var g in groups)
-            {
-                Console.WriteLine("Classification: " + g.Classification);
+            Console.WriteLine("EAGER LOADING");
+            foreach (var v in videosWithGenres)
+                Console.WriteLine("{0} ({1})", v.Name, v.Genre.Name);
 
-                foreach (var v in g.Videos)
-                    Console.WriteLine("\t" + v.Name);
-            }
+            // Explicit loading
 
-            // Classifications and number of videos in them
-            var classifications = context.Videos
-                .GroupBy(v => v.Classification)
-                .Select(g => new
-                {
-                    Name = g.Key.ToString(),
-                    VideosCount = g.Count()
-                })
-                .OrderBy(c => c.Name);
+            // NOTE: At this point, genres are already loaded into the context,
+            // so the following line is not going to make a difference. If you 
+            // want to see expicit loading in action, comment out the eager loading 
+            // part as well as the foreach block in the lazy loading.
+            context.Genres.Load();
 
             Console.WriteLine();
-            Console.WriteLine("CLASSIFICATIONS AND NUMBER OF VIDEOS IN THEM");
-            foreach (var c in classifications)
-                Console.WriteLine("{0} ({1})", c.Name, c.VideosCount);
-
-
-            // Genres and number of videos in them
-            var genres = context.Genres
-                .GroupJoin(context.Videos, g => g.Id, v => v.GenreId, (genre, videos) => new
-                {
-                    Name = genre.Name,
-                    VideosCount = videos.Count()
-                })
-                .OrderByDescending(g => g.VideosCount);
-
-            Console.WriteLine();
-            Console.WriteLine("GENRES AND NUMBER OF VIDEOS IN THEM");
-            foreach (var g in genres)
-                Console.WriteLine("{0} ({1})", g.Name, g.VideosCount);
+            Console.WriteLine("EXPLICIT LOADING");
+            foreach (var v in videos)
+                Console.WriteLine("{0} ({1})", v.Name, v.Genre.Name);
+        
 
 
         }
