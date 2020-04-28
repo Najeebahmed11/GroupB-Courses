753df4c5867f553b89c73c6e27948f8782d20111
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-28 10:23:03 +0500

    Completed Lesson 47. LINQ in Action

---
+++ GroupB-Courses/Queries/Queries/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+            //linq syntax
+            var query =
+                from c in context.Courses
+                where c.Name.Contains("c#")
+                orderby c.Name
+                select c;
+           // foreach(var course in query)
+            //{
+              //  Console.WriteLine(course.Name);
+            //}
+            //Extension method
+            var courses = context.Courses
+                .Where(c => c.Name.Contains("c#"))
+                .OrderBy(c => c.Name);
+            foreach(var course in courses)
+            {
+                Console.WriteLine(course.Name);
+            }
+        }
+    }
+}
