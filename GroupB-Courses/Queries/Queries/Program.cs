4eb3df8cad24b6a13ee608971a8b998a1c087ce8
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-28 12:18:02 +0500

    Completed Lesson 49. LINQ Extension Methods

--- GroupB-Courses/Queries/Queries/Program.cs
+++ GroupB-Courses/Queries/Queries/Program.cs
@@ -10,14 +10,11 @@ namespace Queries
         static void Main(string[] args)
         {
             var Context = new PlutoContext();
-            var query =
-                from a in Context.Authors
-                join c in Context.Courses on a.Id equals c.Author.Id into g
-                select new { AuthorName = a.Name, Courses = g.Count() };
-            foreach(var x in query)
-            {
-                Console.WriteLine("{0}({1})", x.AuthorName, x.Courses);
-            }
+            Context.Courses
+                .Where(c => c.Level == 1)
+                .OrderBy(a=>a.Name)
+                .SelectMany(c=>c.Tags);
+
         }
     }
 }
