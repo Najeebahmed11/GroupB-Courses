9a4704583e6e333695a863e2fd214cb34e0bd4ef
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-29 10:40:47 +0500

    Completed Lesson 12. Projection Operations: Select and SelectMany

--- GroupB-Courses/LINQ/Program.cs
+++ GroupB-Courses/LINQ/Program.cs
@@ -10,29 +10,28 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            var list = new ArrayList();
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            // this won't work
-            //Console.WriteLine(list.Select(i => (int)i).Sum());
-            Console.WriteLine(list.Cast<int>().Sum());
+            var numbers = Enumerable.Range(1, 4);
+            var squares = numbers.Select(x => x * x);
+            Console.WriteLine(squares);
 
-            // cast not valid
-            //Console.WriteLine(list.Cast<float>().Average());
+            string sentence = "This is a nice sentence";
+            var wordLengths = sentence.Split().Select(w => w.Length);
+            Console.WriteLine(wordLengths);
 
-            // materializing collections
-            var numbers = Enumerable.Range(1, 10);
-            var arr = numbers.ToArray(); // ToList()
-            var dict = numbers.ToDictionary(i => (double)i / 10, i => i % 2 == 0);
-            Console.WriteLine(dict);
-            // materializing an infinite sequence is a bad idea
+            // keep both word and length
+            var wordsWithLength = sentence.Split().Select(w => new { Word = w, Size = w.Length });
+            Console.WriteLine(wordsWithLength);
 
-            //
-            var arr2 = new[] { 1, 2, 3 };
-            var arre = arr2.AsEnumerable(); // simply casts to IEnumerable<int>
-            ParallelQuery<int> pq = arr2.AsParallel();
-            // AsQueryable()
+            // get all the words
+            var sequences = new[] { "red,green,blue", "orange", "white,pink" };
+            var allWords = sequences.SelectMany(s => s.Split(',')); // select
+            Console.WriteLine(allWords);
+
+            // select each pair (cross product) from two collections
+            string[] objects = { "house", "car", "bicycle" };
+            string[] colors = { "red", "green", "gray" };
+            var pairs = colors.SelectMany(z => objects, (c, o) => $"{c} {o}");
+            Console.WriteLine(pairs);
         }
     }
 }
