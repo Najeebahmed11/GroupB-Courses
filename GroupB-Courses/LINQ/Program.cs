b6f4548aa667c16c89ee193e7b7fdf3e142d2fee
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-29 10:53:13 +0500

    Completed Lesson 13. Filtering Data: OfType and Where

--- GroupB-Courses/LINQ/Program.cs
+++ GroupB-Courses/LINQ/Program.cs
@@ -10,28 +10,18 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            var numbers = Enumerable.Range(1, 4);
-            var squares = numbers.Select(x => x * x);
-            Console.WriteLine(squares);
+            var numbers = Enumerable.Range(1, 10);
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+            Console.WriteLine(evenNumbers);
 
-            string sentence = "This is a nice sentence";
-            var wordLengths = sentence.Split().Select(w => w.Length);
-            Console.WriteLine(wordLengths);
+            // combine projection and filtering
+            var oddSquares = numbers.Select(x => x * x).Where(y => y % 2 != 0);
+            Console.WriteLine(oddSquares);
 
-            // keep both word and length
-            var wordsWithLength = sentence.Split().Select(w => new { Word = w, Size = w.Length });
-            Console.WriteLine(wordsWithLength);
-
-            // get all the words
-            var sequences = new[] { "red,green,blue", "orange", "white,pink" };
-            var allWords = sequences.SelectMany(s => s.Split(',')); // select
-            Console.WriteLine(allWords);
-
-            // select each pair (cross product) from two collections
-            string[] objects = { "house", "car", "bicycle" };
-            string[] colors = { "red", "green", "gray" };
-            var pairs = colors.SelectMany(z => objects, (c, o) => $"{c} {o}");
-            Console.WriteLine(pairs);
+            // filter by type
+            object[] values = { 1, 2.5, 3, 4.56 };
+            var wholeNumbers = values.OfType<int>(); // try float or double
+            Console.WriteLine(wholeNumbers);
         }
     }
 }
