4d2bb2aab476abbd693d1b6d625cc4b422793520
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-29 09:41:26 +0500

    Completed Lesson 9. Overview

--- GroupB-Courses/LINQ/Program.cs
+++ GroupB-Courses/LINQ/Program.cs
@@ -10,7 +10,15 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-
+            //9.Overview
+            //some operators itrates on entire collection as soon as they are invoked
+            //this is called immediate itration
+            //e.g count(),ToArray()
+            //some operators are defered 
+            //cal will happen when when uh itrate the collection
+            //straming defered operation
+            //they dont hav to read entire adatset
+            //
         }
     }
 }
