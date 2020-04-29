599d3c98674cb4f25681f8e097a2a6c66f56bbee
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-29 10:07:40 +0500

    Completed Lesson 11. Converting Data Types: Cast, ToXxx and AsXxx

--- GroupB-Courses/LINQ/Program.cs
+++ GroupB-Courses/LINQ/Program.cs
@@ -10,16 +10,29 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            // this won't work
+            //Console.WriteLine(list.Select(i => (int)i).Sum());
+            Console.WriteLine(list.Cast<int>().Sum());
 
-            //9.Overview
-            //some operators itrates on entire collection as soon as they are invoked
-            //this is called immediate itration
-            //e.g count(),ToArray()
-            //some operators are defered 
-            //cal will happen when when uh itrate the collection
-            //straming defered operation
-            //they dont hav to read entire adatset
+            // cast not valid
+            //Console.WriteLine(list.Cast<float>().Average());
+
+            // materializing collections
+            var numbers = Enumerable.Range(1, 10);
+            var arr = numbers.ToArray(); // ToList()
+            var dict = numbers.ToDictionary(i => (double)i / 10, i => i % 2 == 0);
+            Console.WriteLine(dict);
+            // materializing an infinite sequence is a bad idea
+
             //
+            var arr2 = new[] { 1, 2, 3 };
+            var arre = arr2.AsEnumerable(); // simply casts to IEnumerable<int>
+            ParallelQuery<int> pq = arr2.AsParallel();
+            // AsQueryable()
         }
     }
 }
