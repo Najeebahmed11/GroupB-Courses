82294a00566b23303d3fe04033b883539d43656d
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-29 18:28:22 +0500

    Commpleted Lesson 32. AsParallel and ParallelQuery

--- GroupB-Courses/LINQ/Program.cs
+++ GroupB-Courses/LINQ/Program.cs
@@ -10,19 +10,27 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            var numbers = Enumerable.Range(1, 10);
-            var evenNumbers = numbers.Where(n => n % 2 == 0);
-            // Console.WriteLine(evenNumbers);
+            const int count = 50;
+            var items = Enumerable.Range(1, count).ToArray();
+            var results = new int[count];
+            items.AsParallel().ForAll(x =>
+            {
+                int newValue = x * x * x;
+                Console.Write($"{newValue}({Task.CurrentId})\t");
+                results[x - 1] = newValue;
+            });
 
-            // combine projection and filtering
-            var Evenquares = evenNumbers.Select(x => x * x);
-            //Console.WriteLine(Evenquares);
-            var Remove = Evenquares.Where(y => y < 50);
-            Console.WriteLine(Remove);
-
-
-
-
+            Console.WriteLine();
+            Console.WriteLine();
+            // foreach(var i in results)
+            //{
+            //  Console.WriteLine($"{i}\t");
+            //Console.WriteLine();
+            //}
+            var cubes = items.AsParallel().AsOrdered().Select(x => x * x * x);
+            foreach (var i in cubes)
+                Console.Write($"{i}\t");
+            Console.WriteLine();
         }
     }
 }
