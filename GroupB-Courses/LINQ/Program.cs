2b8d2766e6367266893ff383e0f5fdd7715701b1
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-29 12:08:34 +0500

    Completed Leson 16. Set Operations: Distinct, Except, Intersect, Union

--- GroupB-Courses/LINQ/Program.cs
+++ GroupB-Courses/LINQ/Program.cs
@@ -10,41 +10,15 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-			void Main()
-			{
-				var people = new List<Person>{
-		new Person { Name = "Adam", Age = 20 },
-		new Person{ Name = "Adam", Age = 36 },
-	  new Person { Name = "Boris", Age = 18 },
-	  new Person { Name = "Claire", Age = 36 },
-	  new Person { Name = "Adam", Age = 20 }, // dup
-	  new Person { Name = "Jack", Age = 20 }
-	};
+            var numbers = Enumerable.Range(1, 10);
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+            // Console.WriteLine(evenNumbers);
 
-				IEnumerable<IGrouping<string, Person>> byName = people.GroupBy(p => p.Name);
-				//Console.WriteLine(byName);
-
-				// IEnumerable<IGrouping<bool, Person>>
-				Console.WriteLine(people.GroupBy(p => p.Age < 30));
-
-				// same, but we only keep the values instead of
-				// entire objects
-				var byAgeNames = people.GroupBy(p => p.Age, p => p.Name);
-				//Console.WriteLine(byAgeNames);
-
-				foreach (var item in byAgeNames)
-				{
-					Console.WriteLine($"These people are {item.Key} years old");
-					foreach (var name in item)
-						Console.WriteLine($" - {name}");
-				}
-			}
-
-class Person
-		{
-			public string Name;
-			public int Age;
-		}
-	}
+            // combine projection and filtering
+            var Evenquares = evenNumbers.Select(x => x * x);
+            //Console.WriteLine(Evenquares);
+            var Remove = Evenquares.Where(y => y < 50);
+            Console.WriteLine(Remove);
+        }
     }
 }
