91f31a38ec4350c7db9bf14697972d6c0a1541ba
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-29 11:53:27 +0500

    Completed Lesson 15. Grouping Data: GroupBy and IGrouping<T>

--- GroupB-Courses/LINQ/Program.cs
+++ GroupB-Courses/LINQ/Program.cs
@@ -12,43 +12,35 @@ namespace LINQ
         {
 			void Main()
 			{
-				var rand = new Random();
-				var randomValues = Enumerable.Range(1, 10).Select(_ => rand.Next(10) - 5);
-
-				var csvString = new Func<IEnumerable<int>, string>(values =>
-				{
-					return string.Join(",", values.Select(v => v.ToString()).ToArray());
-				});
-
-				// different set of values each time
-				Console.WriteLine(csvString(randomValues));
-				Console.WriteLine(csvString(randomValues.OrderBy(x => x)));
-				Console.WriteLine(csvString(randomValues.OrderByDescending(x => x)));
-
-				var people = new List<Person>
-	{
-	  new Person{ Name = "Adam", Age = 36 },
+				var people = new List<Person>{
+		new Person { Name = "Adam", Age = 20 },
+		new Person{ Name = "Adam", Age = 36 },
 	  new Person { Name = "Boris", Age = 18 },
 	  new Person { Name = "Claire", Age = 36 },
-	  new Person { Name = "Adam", Age = 20 },
+	  new Person { Name = "Adam", Age = 20 }, // dup
 	  new Person { Name = "Jack", Age = 20 }
 	};
 
-				Console.WriteLine(people);
-				Console.WriteLine(people.OrderBy(p => p.Name));
+				IEnumerable<IGrouping<string, Person>> byName = people.GroupBy(p => p.Name);
+				//Console.WriteLine(byName);
 
-				// result is IOrderedEnumerable<Person>
+				// IEnumerable<IGrouping<bool, Person>>
+				Console.WriteLine(people.GroupBy(p => p.Age < 30));
 
-				Console.WriteLine(people.OrderBy(p => p.Age)
-										.ThenByDescending(p => p.Name));
+				// same, but we only keep the values instead of
+				// entire objects
+				var byAgeNames = people.GroupBy(p => p.Age, p => p.Name);
+				//Console.WriteLine(byAgeNames);
 
-				// reverse a string
-				string s = "This is a test";
-				Console.WriteLine(new string(s.Reverse().ToArray()));
+				foreach (var item in byAgeNames)
+				{
+					Console.WriteLine($"These people are {item.Key} years old");
+					foreach (var name in item)
+						Console.WriteLine($" - {name}");
+				}
 			}
 
-// Define other methods and classes here
-		class Person
+class Person
 		{
 			public string Name;
 			public int Age;
