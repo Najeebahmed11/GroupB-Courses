abfdab30301885fb378206724c0b43aa9e3062d0
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-30 13:33:44 +0500

    Completed Leson 21. Black-box Testing

--- GroupB-Courses/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ GroupB-Courses/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -8,13 +8,33 @@ namespace TestNinja.UnitTests
     [TestFixture]
     class MathTests
     {
+        [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
             var math = new Math();
             var number = math.Add(1, 2);
             Assert.That(number, Is.EqualTo(3));
-
-
+        }
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgumnet()
+        {
+            var math = new Math();
+            var result=math.Max(2, 1);
+            Assert.That(result, Is.EqualTo(2));
+        }
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgumnet()
+        {
+            var math = new Math();
+            var result = math.Max(1,2);
+            Assert.That(result, Is.EqualTo(2));
+        }
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnSameArgumnet()
+        {
+            var math = new Math();
+            var result = math.Max(1, 1);
+            Assert.That(result, Is.EqualTo(1));
         }
     }
 }
