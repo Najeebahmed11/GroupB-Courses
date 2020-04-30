a359f0c3675a3dae2b5aac44f56af41e893057bb
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-30 13:39:41 +0500

    Complete Lesson 22. Set Up and Tear Down

--- GroupB-Courses/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ GroupB-Courses/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -6,34 +6,38 @@ using Math = TestNinja.Fundamentals.Math;
 namespace TestNinja.UnitTests
 {
     [TestFixture]
-    class MathTests
+    public class MathTests
+        //setup
+        //teardown
     {
+        private Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            var math = new Math();
-            var number = math.Add(1, 2);
+            var number = _math.Add(1, 2);
             Assert.That(number, Is.EqualTo(3));
         }
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnFirstArgumnet()
         {
-            var math = new Math();
-            var result=math.Max(2, 1);
+            var result=_math.Max(2, 1);
             Assert.That(result, Is.EqualTo(2));
         }
         [Test]
         public void Max_SecondArgumentIsGreater_ReturnSecondArgumnet()
         {
-            var math = new Math();
-            var result = math.Max(1,2);
+            var result = _math.Max(1,2);
             Assert.That(result, Is.EqualTo(2));
         }
         [Test]
         public void Max_ArgumentsAreEqual_ReturnSameArgumnet()
         {
-            var math = new Math();
-            var result = math.Max(1, 1);
+            var result = _math.Max(1, 1);
             Assert.That(result, Is.EqualTo(1));
         }
     }
