a2cb7b65e7e62782968e5003dd930fd0a0e4cf07
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-05-01 14:50:50 +0500

    Completed  Lesson 42. Solution- DemeritPointsCalculator

---
+++ GroupB-Courses/source-code-starter/TestNinja/TestNinja.UnitTests/DemerritPointCalculatorTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemerritPointCalculatorTest
+    {
+        [Test]
+        public void DemerritPountCalculator_On_Speed_Zero()
+        {
+            var log = new DemeritPointsCalculator();
+            Assert.That(()=>log.CalculateDemeritPoints(-1),Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+    }
+}
