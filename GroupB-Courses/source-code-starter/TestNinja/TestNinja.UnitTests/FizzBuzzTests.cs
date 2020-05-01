76dfb5eeaf751a491e1eb21bfb137bae92d6ffc3
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-05-01 11:20:35 +0500

    Completed Lesson 39. 19- Exercise- FizzBuzz

---
+++ GroupB-Courses/source-code-starter/TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void GetOutput_Is_a_String()
+        {
+            var fizz = new FizzBuzz();
+            var reult=fizz.GetOutput(34);
+            Assert.That(reult, Is.TypeOf<string>());
+        }
+    }
+}
