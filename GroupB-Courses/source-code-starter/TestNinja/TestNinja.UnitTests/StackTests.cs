a0c3b7dce04af35c2c9bc0fbca6f25a99ad74c71
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-05-01 15:20:03 +0500

    CompleTed Lesson 44. Solution- Stack

---
+++ GroupB-Courses/source-code-starter/TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class StackTests
+    {
+        [Test]
+        public void Push_ArgumentIsNull_ThrowArgumentNullException()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+
+        }
+        [Test]
+        public void Push_ValidArgumnet_AddTheObjectToTheStack()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            stack.Push("a");
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+    }
+}
