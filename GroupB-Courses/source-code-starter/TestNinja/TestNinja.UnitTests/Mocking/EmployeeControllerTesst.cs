af2c0d090e24f6c5fa1bc510d747522baca40de1
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-05-02 18:20:05 +0500

    Completed Lesson 67. Testing EmployeeController

---
+++ GroupB-Courses/source-code-starter/TestNinja/TestNinja.UnitTests/Mocking/EmployeeControllerTesst.cs
@@ -0,0 +1,26 @@
+﻿
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class EmployeeControllerTesst
+    {
+        [Test]
+        public void DeleteEmplyee_WhenCalled_DeleteTheEmployeeFromDataBase()
+        {
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+            controller.DeleteEmployee(1);
+            storage.Verify(s => s.DeleteEmplyee(1));
+        }
+
+    }
+}
